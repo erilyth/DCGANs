ae553fa5d22311c985260bd268b1d2d18f560473
erilyth <vishal.batchu@students.iiit.ac.in>
2016-12-25 00:49:44 +0530

    Tweak model and replace relu activations

--- dcgan.py
+++ dcgan.py
@@ -1,8 +1,8 @@
 import numpy as np
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Input, Convolution2D, Dropout, Flatten, Dense, BatchNormalization, Reshape, UpSampling2D
-from keras.optimizers import Adam
+from keras.layers import Input, Convolution2D, Dropout, Flatten, Dense, BatchNormalization, Reshape, UpSampling2D, Activation
+from keras.optimizers import Adam, sgd
 from keras.layers.advanced_activations import LeakyReLU
 import time
 import matplotlib.pyplot as plt
@@ -15,9 +15,8 @@ from tqdm import tqdm
 discriminator_losses = []
 generator_losses = []
 display_update = 10 # Save the models and update outputs every 5 iterations
-backup_update = 17 # Store a backup of the models every 15 iterations
+backup_update = 400 # Store a backup of the models every 400 iterations
 load_models = 1
-backup2_update = 200
 
 
 def normalize_data(data):
@@ -53,18 +52,18 @@ print(train_data.shape[1:])
 
 # Discriminator Model
 discriminator = Sequential()
-discriminator.add(Convolution2D(128, 5, 5, subsample=(2,2), border_mode='same', activation='relu', input_shape=(1,28,28)))
-discriminator.add(LeakyReLU(0.2))
+discriminator.add(Convolution2D(128, 5, 5, subsample=(2,2), border_mode='same', input_shape=(1,28,28)))
+discriminator.add(Activation('tanh'))
 discriminator.add(Dropout(0.2))
-discriminator.add(Convolution2D(512, 5, 5, subsample=(2,2), border_mode='same', activation='relu'))
-discriminator.add(LeakyReLU(0.2))
+discriminator.add(Convolution2D(512, 5, 5, subsample=(2,2), border_mode='same'))
+discriminator.add(Activation('tanh'))
 discriminator.add(Dropout(0.2))
 discriminator.add(Flatten())
 discriminator.add(Dense(512))
-discriminator.add(LeakyReLU(0.2))
+discriminator.add(Activation('tanh'))
 discriminator.add(Dropout(0.2))
 discriminator.add(Dense(2, activation='softmax'))
-discriminator_optim = Adam(lr=0.0002)
+discriminator_optim = Adam(lr=0.000013)
 # Apply categorical loss at the output of shape (_, 2)
 discriminator.compile(loss='categorical_crossentropy', optimizer=discriminator_optim)
 print(discriminator.summary())
@@ -72,17 +71,17 @@ print(discriminator.summary())
 # Generator Model
 generator = Sequential()
 generator.add(Dense(512*7*7, input_shape=(100,)))
-generator.add(LeakyReLU(0.1))
+generator.add(Activation('tanh'))
 generator.add(Reshape([512, 7, 7]))
 generator.add(UpSampling2D(size=(2, 2)))
-generator.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu'))
-generator.add(LeakyReLU(0.1))
+generator.add(Convolution2D(256, 3, 3, border_mode='same'))
+generator.add(Activation('tanh'))
 generator.add(UpSampling2D(size=(2, 2)))
-generator.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu'))
-generator.add(LeakyReLU(0.1))
-generator.add(Convolution2D(1, 1, 1, border_mode='same', activation='relu'))
-generator.add(LeakyReLU(0.1))
-generator_optim = Adam(lr=0.0003)
+generator.add(Convolution2D(128, 3, 3, border_mode='same'))
+generator.add(Activation('tanh'))
+generator.add(Convolution2D(1, 1, 1, border_mode='same'))
+generator.add(Activation('tanh'))
+generator_optim = Adam(lr=0.00001)
 # Apply log loss at the output of shape (_, 1, 28, 28)
 generator.compile(loss='binary_crossentropy', optimizer=generator_optim)
 print(generator.summary())
@@ -91,7 +90,7 @@ print(generator.summary())
 GAN = Sequential()
 GAN.add(generator)
 GAN.add(discriminator)
-GAN_optim = Adam(lr=0.0002)
+GAN_optim = Adam(lr=0.00001)
 GAN.compile(loss='categorical_crossentropy', optimizer=GAN_optim)
 GAN.summary()
 
@@ -150,9 +149,6 @@ def train_gan():
             discriminator.save_weights("discriminator.keras")
             generator.save_weights("generator.keras")
         if time_step % backup_update == 0:
-            discriminator.save_weights("discriminator_backup.keras")
-            generator.save_weights("generator_backup.keras")
-        if time_step % backup2_update == 0:
             discriminator.save_weights("discriminator_backup-" + str(time_step) + ".keras")
             generator.save_weights("generator_backup-" + str(time_step) + ".keras")
 
